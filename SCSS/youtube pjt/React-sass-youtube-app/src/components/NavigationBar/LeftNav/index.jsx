f57fdd157b8ddc6884e4b4fa4167fba5db50c7be
helen <helen.im@d2.co.kr>
2024-08-19 11:21:09 +0900

    LeftNav

--- SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/LeftNav/index.jsx
+++ SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/LeftNav/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { IoMenu } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import imgUrl from "../../../assets/logo.png";
 
 function LeftNav() {
-  return <div></div>;
+  return (
+    <div className="menu-logo">
+      <button className="icon--container burgerMenu">
+        <IoMenu size={25} />
+      </button>
+
+      <div className="logo-container">
+        <Link>
+          <img src={imgUrl} alt="youtube logo" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default LeftNav;
