905d0987eb5a717d769362d6f239fa18563ac2eb
helen <helen.im@d2.co.kr>
2024-08-19 11:26:49 +0900

    SearchBar

--- SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/SearchBar/index.jsx
+++ SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/SearchBar/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ImSearch } from "react-icons/im";
+import { MdKeyboardVoice } from "react-icons/md";
 
 function SearchBar() {
-  return <div></div>;
+  return (
+    <div className="SearchBar">
+      <form>
+        <input type="text" name="search" placeholder="Search" />
+        <button type="submit">
+          <ImSearch size={20} />
+        </button>
+      </form>
+      <button className="icon-container voiceIcon">
+        <MdKeyboardVoice size={25} />
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
