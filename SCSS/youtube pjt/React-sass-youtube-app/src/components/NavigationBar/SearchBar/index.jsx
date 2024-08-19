20b1cd24fae215e04f54b399e42e91d32f7d9130
helen <helen.im@d2.co.kr>
2024-08-19 16:02:37 +0900

    SearchBar-useWindowSize

--- SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/SearchBar/index.jsx
+++ SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/SearchBar/index.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import { ImSearch } from "react-icons/im";
 import { MdKeyboardVoice } from "react-icons/md";
+import useWindowSize from "../../../helpers/useWindowSize";
 
 function SearchBar() {
+  const { width } = useWindowSize();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // Handle search logic here
+  };
+
+  const handleSearchClick = () => {
+    // Handle search click for small screens
+  };
+
   return (
-    <div className="SearchBar">
-      <form>
-        <input type="text" name="search" placeholder="Search" />
-        <button type="submit">
-          <ImSearch size={20} />
+    <div className={`SearchBar ${width <= 640 ? "smallSearch" : ""}`}>
+      {width > 640 ? (
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+            autoComplete="off"
+          />
+          <button type="submit">
+            <ImSearch size={20} data-tip="search" data-for="navbar" />
+          </button>
+        </form>
+      ) : (
+        <button
+          className="icon-container searchIcon"
+          onClick={handleSearchClick}
+        >
+          <ImSearch size={20} data-tip="Search" data-for="navbar" />
         </button>
-      </form>
+      )}
       <button className="icon-container voiceIcon">
-        <MdKeyboardVoice size={25} />
+        <MdKeyboardVoice
+          size={25}
+          data-tip="Search with your voice"
+          data-for="navbar"
+        />
       </button>
     </div>
   );
