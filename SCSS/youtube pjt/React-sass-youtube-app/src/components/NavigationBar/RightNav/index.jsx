6f7e8d35d55a3901503d48882cb5d726686f913f
helen <helen.im@d2.co.kr>
2024-08-19 11:45:27 +0900

    RightNav, ReactTooltip 제외

--- SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/RightNav/index.jsx
+++ SCSS/youtube pjt/React-sass-youtube-app/src/components/NavigationBar/RightNav/index.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { MdApps, MdNotifications, MdVideoCall } from "react-icons/md";
+import { MdVideoCall as VideoIcon } from "react-icons/md";
+import { MdApps as Apps } from "react-icons/md";
+import { MdNotifications as Notification } from "react-icons/md";
+// import ReactTooltip from "react-tooltip";
 
-function RightNav() {
+const RightNav = () => {
   return (
     <div className="buttons">
       <button className="icon-container">
-        <MdVideoCall size={25} />
+        <VideoIcon size={25} data-tip="Create" data-for="navbar" />
       </button>
 
       <button className="icon-container">
-        <MdApps size={25} />
+        <Apps size={25} data-tip="YouTube Apps" data-for="navbar" />
       </button>
 
       <button className="icon-container">
-        <MdNotifications size={25} />
+        <Notification size={25} data-tip="Notifications" data-for="navbar" />
       </button>
+
+      {/* <ReactTooltip
+        id="navbar"
+        backgroundColor="grey"
+        effect="solid"
+        delayHide={150}
+        arrowColor="transparent"
+      /> */}
     </div>
   );
-}
+};
 
 export default RightNav;
